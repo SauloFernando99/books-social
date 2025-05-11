4505d80f47a54c6a7fa53b34de125e4315de9fd6
SauloFernando99 <saulofernando114@gmail.com>
2025-05-11 10:14:33 -0300

    :tada: initial commit

---
+++ src/test/java/com/example/books_social/BooksSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.books_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
