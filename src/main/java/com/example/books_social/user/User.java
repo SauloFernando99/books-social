c0bf09792625c6ba4743e7f34f08716b1c7303d5
SauloFernando99 <saulofernando114@gmail.com>
2025-05-13 19:37:25 -0300

    :sparkles: feat: created user class and sql database for user:

---
+++ src/main/java/com/example/books_social/user/User.java
@@ -0,0 +1,28 @@
+    package com.example.books_social.user;
+
+    import com.fasterxml.jackson.annotation.JsonAutoDetect;
+    import jakarta.persistence.*;
+    import lombok.AllArgsConstructor;
+    import lombok.EqualsAndHashCode;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+
+    @Table(name = "users")
+    @Entity(name = "User")
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode(of = "id")
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String email;
+        private String password;
+
+        private String userName;
+        private String userPicture;
+
+    }
