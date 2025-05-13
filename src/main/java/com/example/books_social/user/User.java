b344e52df30eff88a838e8c474544c00b3c46031
SauloFernando99 <saulofernando114@gmail.com>
2025-05-13 20:22:37 -0300

    :sparkels: feat: User implements UserDetails class

--- src/main/java/com/example/books_social/user/User.java
+++ src/main/java/com/example/books_social/user/User.java
@@ -6,6 +6,12 @@
     import lombok.EqualsAndHashCode;
     import lombok.Getter;
     import lombok.NoArgsConstructor;
+    import org.springframework.security.core.GrantedAuthority;
+    import org.springframework.security.core.authority.SimpleGrantedAuthority;
+    import org.springframework.security.core.userdetails.UserDetails;
+
+    import java.util.Collection;
+    import java.util.List;
 
     @Table(name = "users")
     @Entity(name = "User")
@@ -14,12 +20,48 @@
     @AllArgsConstructor
     @EqualsAndHashCode(of = "id")
     @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-    public class User {
+    public class User implements UserDetails {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String email;
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public String getUsername() {
+            return email;
+        }
+        // TODO verify account related details
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
         private String password;
         private String userName;
         private String userPicture;
