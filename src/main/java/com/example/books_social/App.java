51657718bfcb1017dc38c751704e752be36231ec
SauloFernando99 <saulofernando114@gmail.com>
2025-05-18 18:01:57 -0300

    :sparkles: feat: updated project to be able to run envroiment variables localy and deployed

--- src/main/java/com/example/books_social/App.java
+++ src/main/java/com/example/books_social/App.java
@@ -1,16 +1,27 @@
 package com.example.books_social;
 
+import com.example.books_social.util.Env;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static com.example.books_social.util.Env.dotenv;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-		System.setProperty("MONGO_ATLAS", dotenv.get("MONGO_ATLAS"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		// MONGO_ATLAS
+		String mongoUri = System.getenv("MONGO_ATLAS");
+		if (mongoUri == null) {
+			mongoUri = Env.get("MONGO_ATLAS");
+		}
+		System.setProperty("MONGO_ATLAS", mongoUri);
+
+		// JWT_SECRET
+		String jwtSecret = System.getenv("JWT_SECRET");
+		if (jwtSecret == null) {
+			jwtSecret = Env.get("JWT_SECRET");
+		}
+		System.setProperty("JWT_SECRET", jwtSecret);
+
 		SpringApplication.run(App.class, args);
 	}
 
