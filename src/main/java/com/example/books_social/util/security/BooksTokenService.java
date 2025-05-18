e8f27b04f5291e97c0df9201396d27e4359a7338
SauloFernando99 <saulofernando114@gmail.com>
2025-05-18 11:09:34 -0300

    :closed_lock_with_key: refactor: updated jwt secret to use Env

--- src/main/java/com/example/books_social/util/security/BooksTokenService.java
+++ src/main/java/com/example/books_social/util/security/BooksTokenService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.books_social.user.User;
+import com.example.books_social.util.Env;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +17,7 @@ import java.time.ZoneOffset;
 @Service
 public class BooksTokenService {
 
-    @Value("${books.password.principal.generation.token}")
-    private String secret;
+    private final String secret = Env.get("JWT_SECRET");
 
     public String generateToken (User user) {
         try {
