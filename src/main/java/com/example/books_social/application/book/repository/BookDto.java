97f5b41eb29e5a7fa27c339376d6bb36f56c8d03
SauloFernando99 <saulofernando114@gmail.com>
2025-05-20 09:06:54 -0300

    :recycle: refactor: moved Book DTO to application repository package

---
+++ src/main/java/com/example/books_social/application/book/repository/BookDto.java
@@ -0,0 +1,26 @@
+package com.example.books_social.application.book.repository;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public record BookDto(
+        UUID bookId,
+        @NotNull @NotBlank UUID ownerId,
+        @NotNull @NotBlank String title,
+        @NotNull @NotBlank String author,
+        @NotNull @NotBlank String genre,
+        @NotNull LocalDate startDate,
+        @NotNull LocalDate endDate,
+        @NotNull @NotBlank String review,
+        @NotNull @NotBlank String favoriteCharacter,
+        @NotNull Integer rating,
+        @NotNull @NotBlank String coverUrl,
+        @NotNull Integer numberPages,
+        @NotNull @NotBlank String readingStatus,
+        @NotNull List<@NotBlank String> bookType,
+        boolean isFavorite
+) {}
