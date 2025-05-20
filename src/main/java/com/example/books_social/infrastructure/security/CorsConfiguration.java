c298da4683af7e6bb334fd68c9fe059e823f7b33
SauloFernando99 <saulofernando114@gmail.com>
2025-05-20 15:52:44 -0300

    :recycle: refactor: modified cors configuration to work with front end

--- src/main/java/com/example/books_social/infrastructure/security/CorsConfiguration.java
+++ src/main/java/com/example/books_social/infrastructure/security/CorsConfiguration.java
@@ -6,18 +6,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
-    @Bean
-    public WebMvcConfigurer corsConfigurer () {
-        return new WebMvcConfigurer() {
+public class CorsConfiguration implements WebMvcConfigurer{
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false);
+                        .allowCredentials(true);
             }
-        };
-    }
+
+
 }
