77e2048a96535513c0df3618c603f678b440633a
SauloFernando99 <saulofernando114@gmail.com>
2025-05-20 17:08:35 -0300

    :fire: refactor: removed db migration

--- src/main/resources/db/migration/V2__create-users-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    user_name VARCHAR(255),
-    user_picture VARCHAR(255)
-);
