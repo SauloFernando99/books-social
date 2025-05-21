79fe7289a946a9357c6972535cdd69229d6ff45d
SauloFernando99 <saulofernando114@gmail.com>
2025-05-21 10:46:50 -0300

    :recycle: refactor: updated imports

--- src/main/java/com/example/books_social/application/book/create/CreateBookPresenter.java
+++ src/main/java/com/example/books_social/application/book/create/CreateBookPresenter.java
@@ -1,6 +1,6 @@
 package com.example.books_social.application.book.create;
 
-import com.example.books_social.application.shared.GenericPresenter;
+import com.example.books_social.application.shared.presenter.GenericPresenter;
 
 public interface CreateBookPresenter extends GenericPresenter<CreateBookService.ResponseModel> {
 }
