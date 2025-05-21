0f46819abf8bafa1ccd36a8160437a05fb8e1c9b
SauloFernando99 <saulofernando114@gmail.com>
2025-05-20 21:16:45 -0300

    :recycle: refactor: renamed BookRepository to MongoBookRepository

--- src/main/java/com/example/books_social/infrastructure/book/MongoBookRepository.java
+++ src/main/java/com/example/books_social/infrastructure/book/MongoBookRepository.java
@@ -1,11 +1,10 @@
 package com.example.books_social.infrastructure.book;
 
-import com.example.books_social.domain.model.book.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BookRepository extends MongoRepository<BookDocument, UUID> {
+public interface MongoBookRepository extends MongoRepository<BookDocument, UUID> {
 }
