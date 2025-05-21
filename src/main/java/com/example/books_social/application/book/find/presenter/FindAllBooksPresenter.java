c6b528f5cc615f9488c7424aff44ecac0a70945d
SauloFernando99 <saulofernando114@gmail.com>
2025-05-21 20:11:33 -0300

    :bug: fix: changed RequestModel to ResponseModel

--- src/main/java/com/example/books_social/application/book/find/presenter/FindAllBooksPresenter.java
+++ src/main/java/com/example/books_social/application/book/find/presenter/FindAllBooksPresenter.java
@@ -3,5 +3,5 @@ package com.example.books_social.application.book.find.presenter;
 import com.example.books_social.application.book.find.services.FindAllBooksService;
 import com.example.books_social.application.shared.presenter.GenericPresenter;
 
-public interface FindAllBooksPresenter extends GenericPresenter<FindAllBooksService.RequestModel> {
+public interface FindAllBooksPresenter extends GenericPresenter<FindAllBooksService.ResponseModel> {
 }
