84e8e4b7863e7f803bf5f3e63fcce65637d63da9
SauloFernando99 <saulofernando114@gmail.com>
2025-05-21 13:35:33 -0300

    :bug: fix: included notation service in uuid generator service implementation

--- src/main/java/com/example/books_social/infrastructure/shared/UuidGeneratorServiceImpl.java
+++ src/main/java/com/example/books_social/infrastructure/shared/UuidGeneratorServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.books_social.infrastructure.shared;
 
 import com.example.books_social.domain.services.UuidGeneratorService;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UuidGeneratorServiceImpl implements UuidGeneratorService {
 
     @Override
