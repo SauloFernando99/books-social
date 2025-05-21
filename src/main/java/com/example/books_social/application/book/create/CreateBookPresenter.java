86adfb6cca7038105535b69dbe07a90014f1153d
SauloFernando99 <saulofernando114@gmail.com>
2025-05-21 08:25:00 -0300

    :sparkles: feat: interfaces for create book presenter and service

---
+++ src/main/java/com/example/books_social/application/book/create/CreateBookPresenter.java
@@ -0,0 +1,6 @@
+package com.example.books_social.application.book.create;
+
+import com.example.books_social.application.shared.GenericPresenter;
+
+public interface CreateBookPresenter extends GenericPresenter<CreateBookService.ResponseModel> {
+}
---
+++ src/main/java/com/example/books_social/application/book/create/CreateBookService.java
@@ -0,0 +1,36 @@
+package com.example.books_social.application.book.create;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface CreateBookService {
+    void createBook(
+        CreateBookPresenter,
+        RequestModel request
+    );
+
+    public record RequestModel (
+        UUID ownerId,
+        String title,
+        String author,
+        String genre,
+        LocalDate startDate,
+        LocalDate endDate,
+        String review,
+        String favoriteCharacter,
+        Integer rating,
+        String coverUrl,
+        Integer numberPages,
+        String readingStatus,
+        List<String> bookTypes,
+        boolean isFavorite
+    ) {}
+
+    public record ResponseModel(
+        UUID bookId,
+        String title,
+        LocalDateTime createdAt
+    ) {}
+}
