edf83a7bfec8a4a1dacf577a734dc4346e6f348c
SauloFernando99 <saulofernando114@gmail.com>
2025-05-21 09:12:26 -0300

    :sparkles: feat: implemented create book function

--- src/main/java/com/example/books_social/application/book/create/CreateBookServiceImpl.java
+++ src/main/java/com/example/books_social/application/book/create/CreateBookServiceImpl.java
@@ -1,2 +1,36 @@
-package com.example.books_social.application.book.create;public class CreateBookServiceImpl {
+package com.example.books_social.application.book.create;
+
+import com.example.books_social.application.book.repository.BookMapper;
+import com.example.books_social.application.book.repository.BookRepository;
+import com.example.books_social.domain.UuidGeneratorService;
+import com.example.books_social.domain.model.book.Book;
+import com.example.books_social.domain.model.book.BookId;
+
+public class CreateBookServiceImpl implements CreateBookService {
+    private final BookRepository bookRepository;
+    private final UuidGeneratorService uuidGeneratorService;
+
+    CreateBookServiceImpl(
+      BookRepository bookRepository,
+      UuidGeneratorService uuidGeneratorService
+    ) {
+        this.bookRepository = bookRepository;
+        this.uuidGeneratorService = uuidGeneratorService;
+    }
+
+    @Override
+    public void createBook(CreateBookPresenter presenter, RequestModel request) {
+        BookId bookId = new BookId(uuidGeneratorService.next());
+        Book book = BookMapper.fromRequestModel(bookId, request);
+
+        bookRepository.create(BookMapper.toDto(book));
+
+        presenter.prepareSuccessView(new ResponseModel(
+            bookId.getValue(),
+            book.getTitle(),
+            book.getCreatedAt()
+        ));
+
+    }
+
 }
