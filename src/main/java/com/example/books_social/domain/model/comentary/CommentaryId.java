4ba972eea4dc82836b115cc58052de6d349b244a
SauloFernando99 <saulofernando114@gmail.com>
2025-05-23 15:08:43 -0300

    :sparkles: feat: implemented commentary id identifier

--- src/main/java/com/example/books_social/domain/model/comentary/CommentaryId.java
+++ src/main/java/com/example/books_social/domain/model/comentary/CommentaryId.java
@@ -1,2 +1,53 @@
-package com.example.books_social.domain.model.comentary;public class CommentaryId {
+package com.example.books_social.domain.model.comentary;
+
+import com.example.books_social.domain.shared.ddd.Identifier;
+import com.example.books_social.domain.shared.ddd.Notification;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CommentaryId implements Identifier<Long> {
+    private final UUID value;
+
+    public CommentaryId(UUID value) {
+        Notification notification = validate(value);
+        if (!notification.hasNoErrors()) {
+            throw new IllegalArgumentException(notification.message());
+        }
+        this.value = value;
+    }
+
+    private Notification validate(UUID value) {
+        Notification notification = new Notification();
+        if (value == null) {
+            notification.addError("Commentary ID must not be null.");
+        }
+        return notification;
+    }
+
+    @Override
+    public Notification validate() {
+        return null;
+    }
+
+    @Override
+    public Long value() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CommentaryId that)) return false;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    public UUID getValue() {
+        return value;
+    }
 }
