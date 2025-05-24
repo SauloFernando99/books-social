2774df0b951fa659e2a4699fe04b44ae3fe76485
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 17:19:12 -0300

    :sparkles: feat: presenter and service interface for find all comments

---
+++ src/main/java/com/example/books_social/application/commentary/find/FindAllCommentsPresenter.java
@@ -0,0 +1,6 @@
+package com.example.books_social.application.commentary.find;
+
+import com.example.books_social.application.shared.presenter.GenericPresenter;
+
+public interface FindAllCommentsPresenter extends GenericPresenter<FindAllCommentsService.ResponseModel> {
+}
---
+++ src/main/java/com/example/books_social/application/commentary/find/FindAllCommentsService.java
@@ -0,0 +1,14 @@
+package com.example.books_social.application.commentary.find;
+
+import com.example.books_social.application.commentary.repository.CommentaryDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FindAllCommentsService {
+    void findAllByBookId(FindAllCommentsPresenter presenter, RequestModel request);
+
+    public record RequestModel(UUID bookId) {}
+
+    public record ResponseModel(UUID ownerId, UUID bookId, Integer numberOfComments, List<CommentaryDto> comments) {}
+}
