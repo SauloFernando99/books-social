e31d8307bc31489487a18da219235f67c6fbbc41
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 12:04:46 -0300

    :sparkles: feat: commentary mapper

---
+++ src/main/java/com/example/books_social/application/commentary/repository/CommentaryMapper.java
@@ -0,0 +1,32 @@
+package com.example.books_social.application.commentary.repository;
+
+import com.example.books_social.domain.model.book.BookId;
+import com.example.books_social.domain.model.comentary.*;
+
+public class CommentaryMapper {
+    public static Commentary fromDto(CommentaryDto dto) {
+        Reaction reaction = Reaction.valueOf(dto.reaction());
+
+        return new Commentary(
+            new CommentaryId(dto.commentaryId()),
+            new BookId(dto.bookId()),
+            dto.userId(),
+            new CommentaryText(dto.commentaryText()),
+            new Progress(dto.progress()),
+            reaction,
+            dto.createdAt()
+        );
+    }
+
+    public static CommentaryDto toDto(Commentary commentary) {
+        return new CommentaryDto(
+            commentary.getCommentaryId().getValue(),
+            commentary.getBookId().getValue(),
+            commentary.getUserId(),
+            commentary.getCommentaryText().toString(),
+            commentary.getProgress().value(),
+            commentary.getReaction().toString(),
+            commentary.getCreatedAt()
+        );
+    }
+}
