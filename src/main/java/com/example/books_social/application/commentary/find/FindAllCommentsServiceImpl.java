9e8440b8feb5a5ba45dd03ea97066419ff93d05e
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 17:41:11 -0300

    :sparkles: feat: find all comments by book service implementation

--- src/main/java/com/example/books_social/application/commentary/find/FindAllCommentsServiceImpl.java
+++ src/main/java/com/example/books_social/application/commentary/find/FindAllCommentsServiceImpl.java
@@ -1,2 +1,40 @@
-package com.example.books_social.application.commentary.find;public class FindAllCommentsServiceImpl {
+package com.example.books_social.application.commentary.find;
+
+import com.example.books_social.application.book.repository.BookRepository;
+import com.example.books_social.application.commentary.repository.CommentaryDto;
+import com.example.books_social.application.commentary.repository.CommentaryRepository;
+import com.example.books_social.application.shared.exceptions.EntityNotFoundException;
+
+import java.util.List;
+
+public class FindAllCommentsServiceImpl implements FindAllCommentsService{
+    private final CommentaryRepository repository;
+    private final BookRepository bookRepository;
+
+    public FindAllCommentsServiceImpl(
+        CommentaryRepository repository,
+        BookRepository bookRepository
+    ) {
+        this.repository = repository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void findAllByBookId(FindAllCommentsPresenter presenter, RequestModel request) {
+        boolean bookExists = bookRepository.existsById(request.bookId());
+
+        if (!bookExists) {
+            String message = "Book of id: " + request.bookId() + "not exists.";
+            presenter.prepareFailView(new EntityNotFoundException(message));
+            return;
+        }
+
+        List<CommentaryDto> comments = repository.findAllCommentsByBook(request.bookId());
+
+        presenter.prepareSuccessView(new ResponseModel(
+            request.bookId(),
+            comments.size(),
+            comments
+        ));
+    }
 }
