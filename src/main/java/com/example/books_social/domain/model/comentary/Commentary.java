36d5e331b8281383e454e3703975f4568db01284
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 11:21:26 -0300

    :sparkles: feat: enum class for reactions

--- src/main/java/com/example/books_social/domain/model/comentary/Commentary.java
+++ src/main/java/com/example/books_social/domain/model/comentary/Commentary.java
@@ -1,8 +1,14 @@
 package com.example.books_social.domain.model.comentary;
 
-import com.example.books_social.domain.shared.ddd.Entity;
-import com.example.books_social.domain.shared.ddd.Identifier;
+import com.example.books_social.domain.model.book.BookId;
+
+import java.util.UUID;
 
 public class Commentary {
+    CommentaryId commentaryId;
+    BookId bookId;
+    UUID ownerId;
+
+
 
 }
---
+++ src/main/java/com/example/books_social/domain/model/comentary/Reaction.java
@@ -0,0 +1,13 @@
+package com.example.books_social.domain.model.comentary;
+
+public enum Reaction {
+    HAPPY,
+    SAD,
+    ANGRY,
+    LOVE,
+    LAUGH,
+    SURPRISED,
+    THINKING,
+    COOL,
+    CRYING
+}
