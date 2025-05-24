bffc033f69caf5e2e303437e52df20940b5f2921
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 13:01:59 -0300

    :wrench: feat: commentary services configuration including create commentary service

--- src/main/java/com/example/books_social/presentation/commentary/controller/CommentaryServicesConfiguration.java
+++ src/main/java/com/example/books_social/presentation/commentary/controller/CommentaryServicesConfiguration.java
@@ -1,2 +1,26 @@
-package com.example.books_social.presentation.commentary.controller;public class CommentaryServicesConfiguration {
+package com.example.books_social.presentation.commentary.controller;
+
+import com.example.books_social.application.book.repository.BookRepository;
+import com.example.books_social.application.commentary.create.CreateCommentaryService;
+import com.example.books_social.application.commentary.repository.CommentaryRepository;
+import com.example.books_social.domain.services.UuidGeneratorService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommentaryServicesConfiguration {
+    CommentaryRepository commentaryRepository;
+    BookRepository bookRepository;
+    UuidGeneratorService uuidGeneratorService;
+
+    @Bean
+    void CreateCommentaryService(
+        CommentaryRepository commentaryRepository,
+        BookRepository bookRepository,
+        UuidGeneratorService uuidGeneratorService
+    ){
+        this.commentaryRepository = commentaryRepository;
+        this.bookRepository = bookRepository;
+        this.uuidGeneratorService = uuidGeneratorService;
+    }
 }
