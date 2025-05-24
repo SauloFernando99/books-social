adcebf27e567e8a622b2e93260d3bd14c698a3f7
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 11:41:47 -0300

    :sparkles: feat: document class for commentary

---
+++ src/main/java/com/example/books_social/infrastructure/commentary/CommentaryDocument.java
@@ -0,0 +1,97 @@
+package com.example.books_social.infrastructure.commentary;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document("commentaries")
+@EqualsAndHashCode(of = "id")
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class CommentaryDocument {
+    @Id
+    private UUID id;
+    private UUID bookId;
+    private UUID userId;
+    private String commentaryText;
+    private Integer progress;
+    private String reaction;
+    private LocalDateTime createdAt;
+
+    public CommentaryDocument(
+        UUID id,
+        UUID bookId,
+        UUID userId,
+        String commentaryText,
+        Integer progress,
+        String reaction,
+        LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.bookId = bookId;
+        this.userId = userId;
+        this.commentaryText = commentaryText;
+        this.progress = progress;
+        this.reaction = reaction;
+        this.createdAt = createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(UUID bookId) {
+        this.bookId = bookId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getCommentaryText() {
+        return commentaryText;
+    }
+
+    public void setCommentaryText(String commentaryText) {
+        this.commentaryText = commentaryText;
+    }
+
+    public Integer getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Integer progress) {
+        this.progress = progress;
+    }
+
+    public String getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(String reaction) {
+        this.reaction = reaction;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
