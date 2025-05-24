cd92a25d690bca21de82ec4dbe805e8e6b24de74
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 12:10:22 -0300

    :sparkles: feat: commentary repository implementation

---
+++ src/main/java/com/example/books_social/infrastructure/commentary/CommentaryRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.example.books_social.infrastructure.commentary;
+
+import com.example.books_social.application.commentary.repository.CommentaryRepository;
+
+public class CommentaryRepositoryImpl implements CommentaryRepository {
+}
