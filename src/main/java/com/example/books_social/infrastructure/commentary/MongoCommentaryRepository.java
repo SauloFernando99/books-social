7267d0764c04eeffa160674bb1f6c58d0f7e0c9c
SauloFernando99 <saulofernando114@gmail.com>
2025-05-24 22:22:18 -0300

    :bug: fix: removed find by id method in mongo commentary repository which was conflicting with another method

--- src/main/java/com/example/books_social/infrastructure/commentary/MongoCommentaryRepository.java
+++ src/main/java/com/example/books_social/infrastructure/commentary/MongoCommentaryRepository.java
@@ -9,5 +9,4 @@ import java.util.UUID;
 @Repository
 public interface MongoCommentaryRepository extends MongoRepository<CommentaryDocument, UUID> {
     List<CommentaryDocument> findAllCommentsByBookId(UUID bookId);
-    CommentaryDocument findByCommentaryId(UUID commentaryId);
 }
