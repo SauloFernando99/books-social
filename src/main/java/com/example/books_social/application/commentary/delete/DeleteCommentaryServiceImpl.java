b009946bc84d312e971b066f3135112f5f960cab
SauloFernando99 <saulofernando114@gmail.com>
2025-05-25 11:38:19 -0300

    :sparkles: feat: delete commentary service implementation

--- src/main/java/com/example/books_social/application/commentary/delete/DeleteCommentaryServiceImpl.java
+++ src/main/java/com/example/books_social/application/commentary/delete/DeleteCommentaryServiceImpl.java
@@ -1,2 +1,27 @@
-package com.example.books_social.application.commentary.delete;public class DeleteCommentaryServiceImpl {
+package com.example.books_social.application.commentary.delete;
+
+import com.example.books_social.application.commentary.repository.CommentaryRepository;
+import com.example.books_social.application.shared.exceptions.EntityNotFoundException;
+
+public class DeleteCommentaryServiceImpl implements DeleteCommentaryService{
+    private final CommentaryRepository repository;
+
+    public DeleteCommentaryServiceImpl(CommentaryRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void deleteCommentary(DeleteCommentaryPresenter presenter, RequestModel request) {
+        boolean exists = repository.existsById(request.commentaryId());
+
+        if (!exists) {
+            String message = "Commentary of id: " + request.commentaryId() + "not exists.";
+            presenter.prepareFailView(new EntityNotFoundException(message));
+            return;
+        }
+
+        repository.deleteById(request.commentaryId());
+
+        presenter.prepareSuccessView(new ResponseModel(request.commentaryId()));
+    }
 }
