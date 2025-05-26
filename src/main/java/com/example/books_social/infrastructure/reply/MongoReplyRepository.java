c5dfb87769ec4d5efc067140ca52c9c796fc8185
SauloFernando99 <saulofernando114@gmail.com>
2025-05-26 08:39:26 -0300

    :sparkles: feat: mongo reply repository

--- src/main/java/com/example/books_social/infrastructure/reply/MongoReplyRepository.java
+++ src/main/java/com/example/books_social/infrastructure/reply/MongoReplyRepository.java
@@ -1,2 +1,8 @@
-package com.example.books_social.infrastructure.reply;public interface MongoReplyRepository {
+package com.example.books_social.infrastructure.reply;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.UUID;
+
+public interface MongoReplyRepository extends MongoRepository<ReplyDocument, UUID> {
 }
