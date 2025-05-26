e74beb6d8b57c0125bc72cfe3b04d6094deaa163
SauloFernando99 <saulofernando114@gmail.com>
2025-05-26 09:31:11 -0300

    :sparkles: feat: reply repository implementation

---
+++ src/main/java/com/example/books_social/infrastructure/reply/ReplyRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.example.books_social.infrastructure.reply;
+
+import com.example.books_social.application.reply.repository.ReplyRepository;
+
+public class ReplyRepositoryImpl implements ReplyRepository {
+}
