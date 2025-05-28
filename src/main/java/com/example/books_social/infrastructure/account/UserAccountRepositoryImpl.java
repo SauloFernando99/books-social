d157c2dd225a456b2ba20a0dd047d19f268a3324
SauloFernando99 <saulofernando114@gmail.com>
2025-05-28 08:51:00 -0300

    :sparkles: feat: user account repository implementation

---
+++ src/main/java/com/example/books_social/infrastructure/account/UserAccountRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.example.books_social.infrastructure.account;
+
+import com.example.books_social.application.account.repository.UserAccountRepository;
+
+public class UserAccountRepositoryImpl implements UserAccountRepository {
+}
