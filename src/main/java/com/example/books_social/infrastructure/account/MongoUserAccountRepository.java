8d6cf3ff7ef92084ef0cb5308d981afff7c8dcd9
SauloFernando99 <saulofernando114@gmail.com>
2025-05-28 10:36:28 -0300

    :adhesive_bandage: fix: included repository annotation on mongo user account repository

--- src/main/java/com/example/books_social/infrastructure/account/MongoUserAccountRepository.java
+++ src/main/java/com/example/books_social/infrastructure/account/MongoUserAccountRepository.java
@@ -1,9 +1,11 @@
 package com.example.books_social.infrastructure.account;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MongoUserAccountRepository extends MongoRepository<UserAccountDocument, UUID> {
     boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/example/books_social/presentation/account/presenter/RestfulCreateUserAccountPresenter.java
@@ -0,0 +1,2 @@
+package com.example.books_social.presentation.account.presenter;public class CreateUserAccountPresenter {
+}
