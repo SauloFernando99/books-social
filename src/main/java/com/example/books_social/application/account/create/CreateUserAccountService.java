c43439c534cbe153e8823f5cb4a1f7843d27245f
SauloFernando99 <saulofernando114@gmail.com>
2025-05-28 10:06:25 -0300

    :sparkles: feat: included create account service interface

--- src/main/java/com/example/books_social/application/account/create/CreateUserAccountService.java
+++ src/main/java/com/example/books_social/application/account/create/CreateUserAccountService.java
@@ -1,5 +1,7 @@
 package com.example.books_social.application.account.create;
 
+import java.util.UUID;
+
 public interface CreateUserAccountService {
     void createUserAccount(
         CreateUserAccountPresenter presenter,
@@ -8,10 +10,15 @@ public interface CreateUserAccountService {
 
     public record RequestModel(
         String username,
-
+        String email,
+        String password,
+        String userPhoto
     ) {}
 
     public record ResponseModel(
-
+        UUID userId,
+        String username,
+        String userEmail,
+        String userPhoto
     ) {}
 }
