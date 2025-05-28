30e6699e32c1865580b19fc1d19ee43691d8fa7d
SauloFernando99 <saulofernando114@gmail.com>
2025-05-28 10:33:52 -0300

    :sparkles: feat: post request for create user account

--- src/main/java/com/example/books_social/presentation/account/requests/PostRequest.java
+++ src/main/java/com/example/books_social/presentation/account/requests/PostRequest.java
@@ -1,2 +1,9 @@
-package com.example.books_social.presentation.account.requests;public record PostRequest() {
+package com.example.books_social.presentation.account.requests;
+
+public record PostRequest(
+    String username,
+    String email,
+    String password,
+    String userPhoto
+) {
 }
