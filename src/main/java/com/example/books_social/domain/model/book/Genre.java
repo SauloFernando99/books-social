2d69220a3cbb90aaca610a7a8af335a6243eb2a0
SauloFernando99 <saulofernando114@gmail.com>
2025-06-01 15:16:56 -0300

    :recycle: refactor: updated genre options

--- src/main/java/com/example/books_social/domain/model/book/Genre.java
+++ src/main/java/com/example/books_social/domain/model/book/Genre.java
@@ -1,9 +1,25 @@
 package com.example.books_social.domain.model.book;
 
 public enum Genre {
-    ACTION,
-    ROMANCE,
     FANTASY,
+    SCIENCE_FICTION,
+    DYSTOPIAN,
+    ADVENTURE,
+    ROMANCE,
     HORROR,
-    DRAMA
+    THRILLER,
+    MYSTERY,
+    HISTORICAL,
+    CLASSIC,
+    DRAMA,
+    POETRY,
+    BIOGRAPHY,
+    HUMOR,
+    TRUE_CRIME,
+    GRAPHIC_NOVEL,
+    YOUNG_ADULT,
+    CHILDREN,
+    TECHNOLOGY,
+    MANGA,
+    COMIC
 }
