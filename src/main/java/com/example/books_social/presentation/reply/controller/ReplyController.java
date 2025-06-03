a7ad5c3a3ccbd1bc811f6524b47ed6150dbd404a
SauloFernando99 <saulofernando114@gmail.com>
2025-06-03 09:11:13 -0300

    :sparkles: feat: get request for find all replies by commentary id

--- src/main/java/com/example/books_social/presentation/reply/controller/ReplyController.java
+++ src/main/java/com/example/books_social/presentation/reply/controller/ReplyController.java
@@ -2,24 +2,31 @@ package com.example.books_social.presentation.reply.controller;
 
 import com.example.books_social.application.reply.create.CreateReplyService;
 import com.example.books_social.application.reply.create.CreateReplyServiceImpl;
+import com.example.books_social.application.reply.find.FindAllRepliesService;
+import com.example.books_social.application.reply.find.FindAllRepliesServiceImpl;
 import com.example.books_social.presentation.reply.presenter.RestfulCreateReplyPresenter;
+import com.example.books_social.presentation.reply.presenter.RestfulFindAllRepliesPresenter;
 import com.example.books_social.presentation.reply.requests.PostRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/reply")
 public class ReplyController {
     private final CreateReplyServiceImpl createReplyService;
+    private final FindAllRepliesServiceImpl findAllRepliesService;
 
     @Autowired
-    public ReplyController(CreateReplyServiceImpl createReplyService) {
+    public ReplyController(
+        CreateReplyServiceImpl createReplyService,
+        FindAllRepliesServiceImpl findAllRepliesService
+    ) {
         this.createReplyService = createReplyService;
+        this.findAllRepliesService = findAllRepliesService;
     }
 
     @PostMapping
@@ -33,6 +40,17 @@ public class ReplyController {
         );
 
         createReplyService.createReply(presenter, model);
+
+        return presenter.getResponseEntity() != null
+                ? presenter.getResponseEntity()
+                : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @GetMapping("/{commentaryId}")
+    public ResponseEntity<?> findAllByCommentaryId(@PathVariable UUID commentaryId) {
+        RestfulFindAllRepliesPresenter presenter = new RestfulFindAllRepliesPresenter();
+
+        findAllRepliesService.findAllByCommentaryId(presenter, new FindAllRepliesService.RequestModel(commentaryId));
 
         return presenter.getResponseEntity() != null
                 ? presenter.getResponseEntity()
