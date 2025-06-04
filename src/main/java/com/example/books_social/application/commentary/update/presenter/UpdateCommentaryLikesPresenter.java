36df4d2c882b38f51d78d9d65d569ec9d57a2e95
SauloFernando99 <saulofernando114@gmail.com>
2025-06-04 08:17:02 -0300

    :sparkles: feat: presenter and service interface for update commentary likes

---
+++ src/main/java/com/example/books_social/application/commentary/update/presenter/UpdateCommentaryLikesPresenter.java
@@ -0,0 +1,7 @@
+package com.example.books_social.application.commentary.update.presenter;
+
+import com.example.books_social.application.commentary.update.service.UpdateCommentaryLikesService;
+import com.example.books_social.application.shared.presenter.GenericPresenter;
+
+public interface UpdateCommentaryLikesPresenter extends GenericPresenter<UpdateCommentaryLikesService.ResponseModel> {
+}
---
+++ src/main/java/com/example/books_social/application/commentary/update/service/UpdateCommentaryLikesService.java
@@ -0,0 +1,24 @@
+package com.example.books_social.application.commentary.update.service;
+
+import com.example.books_social.application.commentary.repository.CommentaryDto;
+import com.example.books_social.application.commentary.update.presenter.UpdateCommentaryLikesPresenter;
+import com.example.books_social.domain.model.comentary.CommentaryId;
+
+import java.util.UUID;
+
+public interface UpdateCommentaryLikesService {
+    void updateLikes(
+        UpdateCommentaryLikesPresenter presenter,
+        RequestModel request
+    );
+
+    public record RequestModel(
+        CommentaryId commentaryId,
+        String action
+    ) {}
+
+    public record ResponseModel(
+       UUID commentaryId,
+       Integer likes
+    ) {}
+}
