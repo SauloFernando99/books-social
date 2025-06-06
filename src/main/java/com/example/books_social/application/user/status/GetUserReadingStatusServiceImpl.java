790c4e1d06fd335da06ec05ac58b821030baae37
SauloFernando99 <saulofernando114@gmail.com>
2025-06-06 09:09:57 -0300

    :sparkles: feat: get user reading status service implementation

--- src/main/java/com/example/books_social/application/user/status/GetUserReadingStatusServiceImpl.java
+++ src/main/java/com/example/books_social/application/user/status/GetUserReadingStatusServiceImpl.java
@@ -1,2 +1,33 @@
-package com.example.books_social.application.user.status;public class GetUserReadingStatusServiceImpl {
+package com.example.books_social.application.user.status;
+
+import com.example.books_social.application.book.repository.BookDto;
+import com.example.books_social.application.book.repository.BookRepository;
+
+public class GetUserReadingStatusServiceImpl implements GetUserReadingStatusService{
+    private final BookRepository bookRepository;
+
+    public GetUserReadingStatusServiceImpl(
+        BookRepository bookRepository
+    ) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void getUserReadingStatus(GetUserReadingStatusPresenter presenter, RequestModel request) {
+        BookDto longestReadBook = bookRepository.findLongestReadBook(request.userId());
+        BookDto shortestReadBook = bookRepository.findShortestReadBook(request.userId());
+        Integer totalRead = bookRepository.findTotalRead(request.userId());
+        Integer readPages = bookRepository.findReadPages(request.userId());
+        String mostReadGenre = bookRepository.findMostReadGenre(request.userId());
+
+        presenter.prepareSuccessView(
+            new ResponseModel(
+                longestReadBook,
+                shortestReadBook,
+                totalRead,
+                readPages,
+                mostReadGenre
+            )
+        );
+    }
 }
